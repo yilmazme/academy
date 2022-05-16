3e81ec6583868301eae5d0604caa2d56de286a4e
yilmazme <10myilmaz@gmail.com>
2022-05-17 00:15:10 +0300

    3d carousel added for courses

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -7,9 +7,25 @@ import videoCrown from "../../asssets/videos/academy-crown.mp4";
 import videoSkate from "../../asssets/videos/academy-Skateboard.mp4";
 import videoQMark from "../../asssets/videos/academy-questionmark.mp4";
 import { useNavigate } from "react-router-dom";
+import { Carousel } from "3d-react-carousal";
 
 function Courses() {
   const navigate = useNavigate();
+
+  let slides = [
+    <div>
+      <video className={styles.video_container} src={videoCrown} controls loading="lazy"></video>
+    </div>,
+    <div>
+      <video className={styles.video_container} src={videoSkate} controls loading="lazy"></video>
+    </div>,
+    <div>
+      <video className={styles.video_container} src={videoQMark} controls loading="lazy"></video>
+    </div>,
+    <div>
+      <video className={styles.video_container} src={videoHeart} controls loading="lazy"></video>
+    </div>,
+  ];
 
   return (
     <div className={styles.main}>
@@ -31,23 +47,9 @@ function Courses() {
           <Link to="/register">register</Link>
         </div>
       </div>
-      <div className={styles.intro}>
-        <h2>Kurslar</h2>
-        <h4>916 Kreatif Ekibinin Öğretici Videoları Yakında Burada Olacak!</h4>
-      </div>
+
       <div className={styles.videos_container}>
-        <div>
-          <video className={styles.video_container} src={videoCrown} controls loading="lazy"></video>
-        </div>
-        <div>
-          <video className={styles.video_container} src={videoSkate} controls loading="lazy"></video>
-        </div>
-        <div>
-          <video className={styles.video_container} src={videoQMark} controls loading="lazy"></video>
-        </div>
-        <div>
-          <video className={styles.video_container} src={videoHeart} controls loading="lazy"></video>
-        </div>
+        <Carousel slides={slides} autoplay={false} />
       </div>
     </div>
   );
