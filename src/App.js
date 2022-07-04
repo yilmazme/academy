5948f67d7275c40324360dbb484c0511a5770edd
yilmazme <10myilmaz@gmail.com>
2022-07-04 22:48:18 +0300

    snackbar removed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
+import React from "react";
 import "./App.css";
 import Courses from "./components/courses/Courses";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import Register from "./components/register/Register";
-import SnackbarProvider from "react-simple-snackbar";
+
 function App() {
   return (
-    <SnackbarProvider>
-      <div className="app">
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Home />}></Route>
-            <Route path="/courses" element={<Courses />}></Route>
-            <Route path="/register" element={<Register />}></Route>
-          </Routes>
-        </Router>
-      </div>
-    </SnackbarProvider>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route path="/courses" element={<Courses />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
